4c72ca26fbcae5c1b78f363cc813dd1ede3b1d17
Abel Gechure <aoroko09@gmail.com>
2020-05-10 18:19:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
+      
     
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
-
+    name = "abel"
+    binding.pry
   end
 end
+
